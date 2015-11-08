59e6a8c1199a24101a28a35f7c431e172e90ee82
Dan Peirce <peirce.dan@gmail.com>
2015-11-07 19:32:45 -0800

    first commit

---
+++ clear.c
@@ -0,0 +1,93 @@
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+// function adapted from 
+// http://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf("error %d from tcgetattr\n", errno);
+                return -1;
+        }
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+                printf("error %d from tcsetattr\n", errno);
+                return -1;
+        }
+        return 0;
+}
+
+// function adapted from 
+// http://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf("error %d from tggetattr\n", errno);
+                return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                printf("error %d setting term attributes\n", errno);
+}
+
+
+int main()
+{
+    char *portname = "/dev/ttyMFD1";
+    char clear[3];
+
+    clear[0] = 0xfe;
+    clear[1] = 0x01;
+
+    int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0)
+    {
+        printf("error %d opening %s: %s\n", errno, portname, strerror (errno));
+        return;
+    }
+
+    set_interface_attribs (fd, B9600, 0);  // set speed to 9600 bps, 8n1 (no parity)
+    set_blocking (fd, 0);                // set no blocking
+
+    write (fd, clear , 2);           // code clear and zero LCD display
+    printf("clearing display \n");
+    close (fd);
+    return 0;
+}
+
